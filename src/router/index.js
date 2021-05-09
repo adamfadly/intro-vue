b31895af179f1d3feb8aacc0dab0f6a7e9abf1ee
Mocha Adam Fadly <adamfadly11@gmail.com>
2021-05-09 22:08:02 +0700

    playing rendering with conditional to display stocks to ui

--- src/router/index.js
+++ src/router/index.js
@@ -21,6 +21,17 @@ const routes = [
         /* webpackChunkName: "AttributeBinding" */ "../views/AttributeBinding.vue"
       ),
   },
+  {
+    path: "/conditional-rendering",
+    name: "Conditional-Rendering",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "ConditionalRendering" */ "../views/ConditionalRendering.vue"
+      ),
+  },
 ];
 
 const router = new VueRouter({
